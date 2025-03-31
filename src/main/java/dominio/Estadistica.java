2e4ed092335dd528f8de10ad0334c2e52730a9c0
evalm116 <em.lopezmedina@um.es>
2025-03-31 02:05:18 +0200

    Creación de y funcionalidad de las estadisticas

---
+++ src/main/java/dominio/Estadistica.java
@@ -0,0 +1,126 @@
+package dominio;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Estadistica {
+	
+	private int totalPreguntasRespondidas;
+    private int totalAciertos;
+    private int totalFallos;
+    private LocalDateTime inicioSesion;
+    private Duration tiempoTotalEstudio;
+    private int mejorRacha; // preguntas seguidas correctas
+    private int rachaActual;
+    private LocalDate ultimoDiaEstudio;
+
+    public Estadistica() {
+        this.tiempoTotalEstudio = Duration.ZERO;
+        this.mejorRacha = 0;
+        this.rachaActual = 0;
+        this.totalAciertos = 0;
+        this.totalPreguntasRespondidas = 0;
+        this.totalFallos=0;
+        this.inicioSesion = LocalDateTime.now();
+        this.ultimoDiaEstudio = null;
+    }
+
+    public int getTotalPreguntasRespondidas() {
+		return totalPreguntasRespondidas;
+	}
+
+	public void setTotalPreguntasRespondidas(int totalPreguntasRespondidas) {
+		this.totalPreguntasRespondidas = totalPreguntasRespondidas;
+	}
+
+	public int getTotalAciertos() {
+		return totalAciertos;
+	}
+
+	public void setTotalAciertos(int totalAciertos) {
+		this.totalAciertos = totalAciertos;
+	}
+
+	public int getTotalFallos() {
+		return totalFallos;
+	}
+
+	public void setTotalFallos(int totalFallos) {
+		this.totalFallos = totalFallos;
+	}
+
+	public LocalDateTime getInicioSesion() {
+		return inicioSesion;
+	}
+
+	public void setInicioSesion(LocalDateTime inicioSesion) {
+		this.inicioSesion = inicioSesion;
+	}
+
+	public Duration getTiempoTotalEstudio() {
+		return tiempoTotalEstudio;
+	}
+
+	public void setTiempoTotalEstudio(Duration tiempoTotalEstudio) {
+		this.tiempoTotalEstudio = tiempoTotalEstudio;
+	}
+
+	public int getMejorRacha() {
+		return mejorRacha;
+	}
+
+	public void setMejorRacha(int mejorRacha) {
+		this.mejorRacha = mejorRacha;
+	}
+
+	public int getRachaActual() {
+		return rachaActual;
+	}
+
+	public void setRachaActual(int rachaActual) {
+		this.rachaActual = rachaActual;
+	}
+
+	public void registrarRespuesta(boolean acierto) {
+		totalPreguntasRespondidas++;
+			
+        if (acierto) {
+            totalAciertos++;
+        } else {
+            totalFallos++;
+        }
+    }
+	
+	public void registrarEstudioHoy() {
+        LocalDate hoy = LocalDate.now();
+
+        if (ultimoDiaEstudio == null) {
+            rachaActual = 1;
+        } else {
+            long diasEntre = ChronoUnit.DAYS.between(ultimoDiaEstudio, hoy);
+
+            if (diasEntre == 1) {
+                rachaActual++;
+            } else if (diasEntre > 1) {
+                rachaActual = 1; 
+            } 
+        }
+
+        if (rachaActual > mejorRacha) {
+            mejorRacha = rachaActual;
+        }
+
+        ultimoDiaEstudio = hoy;
+	}
+
+    public void finalizarSesion() {
+    	 if (inicioSesion != null) {
+    	        Duration duracion = Duration.between(inicioSesion, LocalDateTime.now());
+    	        tiempoTotalEstudio = tiempoTotalEstudio.plus(duracion);
+    	        inicioSesion = null; // Marcar como cerrada esta sesión
+    	    }
+    }
+
+}
